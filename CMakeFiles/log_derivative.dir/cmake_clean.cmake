a3b08140eff318ef28c95a7ba16ac4251c191e44
artfin <artfin@mail.ru>
2018-12-05 17:02:42 +0300

    interim

---
+++ CMakeFiles/log_derivative.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/log_derivative.dir/log_derivative.cpp.o"
+  "CMakeFiles/log_derivative.dir/log.cpp.o"
+  "log_derivative.pdb"
+  "log_derivative"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/log_derivative.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- log_derivative.cpp
+++ log_derivative.cpp
@@ -10,7 +10,12 @@
 template<class T> void __log__::value_of(const std::string& name, const T& value) 
 {
     fill_wsp();
-    std::cout << wsp << name << " (" << demangled_type_info_name(typeid(value)) << ") = [" << value << "]" << std::endl;
+    std::string tp = demangled_type_info_name(typeid(value));
+    std::size_t found = tp.find("Eigen");
+    if ( found != std::string::npos )
+        std::cout << wsp << name << " (" << demangled_type_info_name(typeid(value)) << ") = \n" << value << std::endl;
+    else
+        std::cout << wsp << name << " (" << demangled_type_info_name(typeid(value)) << ") = [" << value << "]" << std::endl;
 }
 
 inline int delta(int i, int j) { return i == j; }
@@ -28,7 +33,7 @@ void potmat(Eigen::MatrixXd & w, const double r, const int nch)
 // 51.82570724
 // 55.80351609
 {
-    assert(nch == 4 && "Example excepts for nch=4");
+    assert(nch == 4 && "Example excepts nch=4");
 
     double cos_r = std::cos(r);
 
@@ -36,16 +41,18 @@ void potmat(Eigen::MatrixXd & w, const double r, const int nch)
     {
         for ( int j = 0; j < nch; ++j )
         {
-            w(i, j) = cos_r / std::max(i, j) + delta(i, j) / std::pow(r, i);
+            w(i, j) = cos_r / std::max(i + 1, j + 1) + delta(i + 1, j + 1) / std::pow(r, i + 1);
         }
     }
 }
 
-void logdb(Eigen::MatrixXd & z, Eigen::MatrixXd & w, Eigen::MatrixXd & wref, const int nch, const double rmin, const double rmax, const int nsteps)
+void logdb(const double Energy, Eigen::MatrixXd & z, Eigen::MatrixXd & w, Eigen::MatrixXd & wref, const int nch, const double rmin, const double rmax, const int nsteps)
 // routine to initialise the log derivative matrix, y, at r = rmin, 
 // and propagate it from rmin to rmax 
 // ----------------------------------------------------------
 // variables in call list:
+// Energy       
+//
 // z            matrix of dimension (nch, nch) 
 //              on return `z` contains the log derivative matrix at r = rmax
 // w            workspace for the potential matrix
@@ -54,18 +61,21 @@ void logdb(Eigen::MatrixXd & z, Eigen::MatrixXd & w, Eigen::MatrixXd & wref, con
 // rmin, rmax   integration range limits
 //              rmin is assummed to lie inside the classically forbidden
 //              region for all channels
-// nsteps       number of sectors partitioning integration range 
+// nsteps       number of sectors partitioning integration range
+//
+// detZ         vector of determinant of log derivative matrix
 {
     DEBUG_METHOD("logdb");
 
-    double h = (rmax - rmin) / nsteps;
+    double h = (rmax - rmin) / (2.0 * nsteps);
     DEBUG_VALUE_OF(h);
 
     double d3 = h / 3.0;
     double d6 = h * h / 6.0;
 
     double r = rmin;
-    potmat(w, r, nch);
+    potmat(Energy, w, r, nch);
+    DEBUG_VALUE_OF(w);
 
     // use diagonal approximation to WKB initial value for log derivative matrix
     // (equation 16)
@@ -78,14 +88,15 @@ void logdb(Eigen::MatrixXd & z, Eigen::MatrixXd & w, Eigen::MatrixXd & wref, con
     double wdiag = 0.0;
     for ( int i = 0; i < nch; ++i )
     {
-        wdiag = w(i, i);
-        if ( wdiag < 0.0 )
-        {
-            std::cout << "DIAGONAL REFERENCE POTENTIAL SHOULD BE POSITIVE! ABORT." << std::endl;
-            exit( 1 );
-        }
+        //wdiag = w(i, i);
+        //if ( wdiag < 0.0 )
+        //{
+            //std::cout << "DIAGONAL REFERENCE POTENTIAL SHOULD BE POSITIVE! ABORT." << std::endl;
+            //exit( 1 );
+        //}
 
-        z(i, i) = std::sqrt(wdiag);
+        //z(i, i) = std::sqrt(wdiag);
+        z(i, i) = 1.0e20;
     }
 
     Eigen::MatrixXd z1 = Eigen::MatrixXd::Zero(nch, nch);
@@ -93,33 +104,27 @@ void logdb(Eigen::MatrixXd & z, Eigen::MatrixXd & w, Eigen::MatrixXd & wref, con
     Eigen::MatrixXd z3 = Eigen::MatrixXd::Zero(nch, nch);
     Eigen::MatrixXd z4 = Eigen::MatrixXd::Zero(nch, nch);
 
+    Eigen::MatrixXd Qa = Eigen::MatrixXd::Zero(nch, nch);
+    Eigen::MatrixXd Qb = Eigen::MatrixXd::Zero(nch, nch);
+    Eigen::MatrixXd Qc = Eigen::MatrixXd::Zero(nch, nch);
+
     int istep = 1;
     // propagate z from rmin to rmax
     for ( int kstep = 1; kstep <= nsteps; ++kstep, istep += 2 )
     {
-        // обнулить z??
-        // eq. 5.61 from Manolopoulos Thesis
-        z += d3 * w; 
-
         // the reference potential for the sector is the diagonal of the
         // coupling matrix evaluated at the centre of the sector, r = c
         // (eqn 15 of Manolopoulos1986)
-        r = rmin + 0.5 * istep * h;
+        r = rmin + istep * h;
         DEBUG_VALUE_OF(r);
         DEBUG_PRINT("Reference potential is evaluated at the centre of the sector (r = c)\n"); 
-        potmat(w, r, nch);
+        potmat(Energy, w, r, nch);
         
         // the reference potential:
         for ( int ich = 0; ich < nch; ++ich )
             wref(ich, ich) = w(ich, ich);
 
-        // adjust quadrature contribution at r = a to account for
-        // sector reference potential
-        // (eqn 11 of Manolopoulos1986)
-        z -= d3 * wref;
-        // итого мы посчитали Q(a) = h/3 U(a) = h/3(W(a) - Wref(a))
-
-        // evaluate half sector propagators (eqn 10, Manolopoulos1986)
+        // evaluate half sector propagators (eqn 10, manolopoulos1986)
         // z1: y1, z2: y2, z3: y3, z4: y4
         double arg;
         for ( int ich = 0; ich < nch; ++ich )
@@ -127,82 +132,95 @@ void logdb(Eigen::MatrixXd & z, Eigen::MatrixXd & w, Eigen::MatrixXd & wref, con
             arg = std::sqrt(std::abs(wref(ich, ich)));
             if ( wref(ich, ich) < 0.0 )
             {
-                double ctn = 1.0 / std::tan(arg);
+                double ctn = 1.0 / std::tan(arg * h);
                 z1(ich, ich) = arg * ctn;
-                double csc = 1.0 / std::sin(arg);
+                double csc = 1.0 / std::sin(arg * h);
                 z2(ich, ich) = arg * csc;
             }
             else
             {
-                double ctnh = 1.0 / std::tanh(arg);
+                double ctnh = 1.0 / std::tanh(arg * h);
                 z1(ich, ich) = arg * ctnh;
-                double csch = 1.0 / std::sinh(arg); 
+                double csch = 1.0 / std::sinh(arg * h); 
                 z2(ich, ich) = arg * csch;
             }
 
             z3(ich, ich) = z2(ich, ich);
             z4(ich, ich) = z1(ich, ich);
         } 
-        
-        // propagate z matrix across the first half sector
-        // from r = a to r = c (eqn 14 of Manolopoulos1986)
-        
-        // Y(c) = z4(a,c) - z3(a,c) * (z(a) + z1(a,c))^(-1) * z2(a,c)
-        z += z1;
-        z.inverse();
-        // z now contains (z(a) + z1(a,c))^(-1)
-        z = z4 - z3 * z * z2;
-        // z now contains z(c)
-        
+        //DEBUG_VALUE_OF(z1);
+        //DEBUG_VALUE_OF(z2);
+
+        // adjust quadrature contribution at r = a to account for
+        // sector reference potential
+        r = rmin + 2.0 * (kstep - 1) * h;
+        potmat(Energy, w, r, nch);
+        Qa = d3 * (w - wref);
+        std::cout << "r: " << r << " for Qa evaluation" << std::endl;
+
         // evaluate quadrature contribution at midpoint r = c 
         // (eqn 12 from Manolopoulos1986)
         // evaluate potential matrix at midpoint r = c
         // матрица w в точке r = c была рассчитана в самом начале цикла
         // при расчете Wref
         // уже можно поганить эту матрицу, так что делаем inplace transformations  
+        r = rmin + istep * h;
+        potmat(Energy, w, r, nch);
+        std::cout << "r: " << r << " for Qc evaluation" << std::endl;
         w *= -d6;
         // U(c) = w(c) - wref(c), т.к. wref вычисляется как диагональная часть w(c), то U(c) имеет нулевые элементы на диагонали
         // а значит I - U(c) содержит единицы на диагонали
         for ( int ich = 0; ich < nch; ++ich )
             w(ich, ich) = 1.0;
         // w now containts I - h^2/6*U(c)
-        w.inverse();
+        w = w.inverse();
         
         // subtracting identity matrix
         for ( int ich = 0; ich < nch; ++ich )
             w(ich, ich) -= 1.0;
 
-        w *= 4.0 / h;
+        Qc = w * 4.0 / h;
         // w now contains Q(c)
+       
+        z1 += Qa;
+        z4 += Qc;
+        //DEBUG_VALUE_OF(z1);
+        //DEBUG_VALUE_OF(z2);
+        //DEBUG_VALUE_OF(z3);
+        //DEBUG_VALUE_OF(z4);
+
+        // propagate z matrix across the first half sector
+        // from r = a to r = c (eqn 14 of Manolopoulos1986)
         
-        // apply quadrature contribution at midpoint
-        // (eqn 13 from Manolopoulos1986)
-        z1 += w; // z1(c, b)
-        z4 += w; // z4(c, b)
-        // z2(c, b) = z2(a, c)
-        // z3(c, b) = z3(a, c)
+        // Y(c) = z4(a,c) - z3(a,c) * (z(a) + z1(a,c))^(-1) * z2(a,c)
+        z += z1;
+        z = z.inverse();
+        // z now contains (z(a) + z1(a,c))^(-1)
+        z = z4 - z3 * z * z2;
+        // z now contains z(c)
+       
+        z1 -= Qa;
+        z4 -= Qc;
+
+        r = rmin + 2.0 * kstep * h;
+        potmat(Energy, w, r, nch);
+        Qb = d3 * (w - wref);
+        std::cout << "r: " << r << " for Qb evaluation" << std::endl;
+
+        z1 += Qc;
+        z4 += Qb; 
 
         // propagate z matrix across the second half sector
         // from r = c to r = b (eqn 14 from Manolopoulos1986)
         
         // Y(b) = z4(c, b) - z3(c, b) * (z(c) + z1(c, b))^(-1) * z2(c, b)
         z += z1;
-        z.inverse();
+        z = z.inverse();
         // z now contains (z(c) + z1(c, b))^(-1)
         z = z4 - z3 * z * z2;
         // z now contains Y(b)
 
-        // apply reference potential adjustment to quadrature at r = b
-        // (eqn 11 from Manolopoulos 1986)
-        z -= d3 * wref;
-
-        r = rmin + kstep * h;
-        std::cout << "(end of loop) r = " << r << std::endl;
-
-        // apply quadrature contribution at r = b
-        // (eqn 12 from Manolopoulos 1986)
-        potmat(w, r, nch);
-        w += d3 * z;
+        DEBUG_VALUE_OF(z);
     }
 }
 
@@ -215,12 +233,14 @@ int main()
     Eigen::MatrixXd w = Eigen::MatrixXd::Zero(nch, nch);
     Eigen::MatrixXd wref = Eigen::MatrixXd::Zero(nch, nch);
     
-    const double rmin = 0.0;
+    const double rmin = 0.1;
     const double rmax = 1.0;
 
-    const int nsteps = 2;
+    const int nsteps = 30;
 
-    logdb(z, w, wref, nch, rmin, rmax, nsteps);
+    const double Energy = 20.0; 
 
+    logdb(Energy, z, w, wref, nch, rmin, rmax, nsteps);
+    
     return 0;
 }
